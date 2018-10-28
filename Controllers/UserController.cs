96ca4d323341400f292ab30a07d7a91da3668195
Bogdan Rotaru <bogdan.rotaru13@yahoo.com>
2018-10-28 16:34:18 +0200

    Trying to make login

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Comert_Electronic.Controllers
+{
+    public class UserController : Controller
+    {
+
+        //[Route("api/[controller]/[action]")]
+        //private const string ControllerName = "User";
+        //private DatabaseContext db;
+
+        //public UserController(DatabaseContext db)
+        //{
+        //    this.db = db;
+        //}
+    }
+
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComertElectronic.Models
+{
+    public class User
+    {
+        [Key]
+        public String Email { get; set; }
+        public String Password { get; set; }
+
+        public User() { }
+
+        public User(String email, String password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
